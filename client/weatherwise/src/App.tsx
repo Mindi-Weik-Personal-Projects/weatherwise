285e052c7cc82a2174edba3de5624a37f897d7de
Mindi Weik <57885108+mindiweik@users.noreply.github.com>
2023-07-19 20:44:43 -0600

    chore: additional initial setup modifications (#2)

--- client/weatherwise/src/App.tsx
+++ client/weatherwise/src/App.tsx
@@ -1,13 +1,14 @@
-import './App.css';
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src="/weatherWise-logo-blue.png" alt="weather wise logo"/>
         <h1>Weather Wise -- REMIX</h1>
       </header>
     </div>
   );
 }
 
-export default App;
+export default App
---
+++ server/infrastructure/open-weather-api.ts
@@ -0,0 +1,10 @@
+import axios from 'axios'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const TOKEN = process.env.TOKEN
+
+// geoCode API doc: https://openweathermap.org/api/geocoding-api
+// forecast API doc: https://openweathermap.org/api/one-call-3
+
