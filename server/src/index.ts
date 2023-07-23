4a34da4e273f34483ed528f95478611a01b0e603
mindiweik <mindiweik@gmail.com>
2023-07-23 11:11:11 -0600

    chore: implement eslint for server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,16 @@
-import express, { Express, Request, Response } from 'express'
+import express, { type Express, type Request, type Response } from 'express'
 import dotenv from 'dotenv'
 
 dotenv.config()
 
 const app: Express = express()
-const port = process.env.PORT || 8000
+const port = process.env.PORT ?? 8000
 
 app.get('/', (req: Request, res: Response) => {
-  res.send(`TEST -- Express + TypeScript Server`)
+  console.log(req, res)
+  res.send('TEST -- Express + TypeScript Server')
 })
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`)
-});
+})
